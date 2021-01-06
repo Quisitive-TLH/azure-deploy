af899c59515e3a2c1df7121602aee763c053bd8c
travishofmeister <travis.hofmeister@gmail.com>
2021-01-06 14:48:11 -0600

    adding in boxstarter packages

---
+++ code/infrastructure/arm/workstation/boxstarter.ps1
@@ -0,0 +1,2 @@
+Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/travis-hofmeister-quisitive/azure-deploy/master/code/infrastructure/arm/workstation/boxstarter-packages.txt
+
