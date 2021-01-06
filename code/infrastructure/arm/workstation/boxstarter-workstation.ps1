9e87e726457586319426735b09ee4cd45f327d74
travishofmeister <travis.hofmeister@gmail.com>
2021-01-06 14:07:08 -0600

    removing comments.

--- code/infrastructure/arm/workstation/boxstarter-workstation.ps1
+++ code/infrastructure/arm/workstation/boxstarter-workstation.ps1
@@ -14,7 +14,7 @@ Update-ExecutionPolicy Unrestricted
 Disable-InternetExplorerESC  #Turns off IE Enhanced Security Configuration that is on by default on Server OS versions
 Disable-UAC  # until this is over
 
-disable-computerrestore -drive "C:\"  # http://ss64.com/ps/disable-computerrestore.html  ** Goes >BANG< on Server 2012 but not fatal.
+disable-computerrestore -drive "C:\"  # http://ss64.com/ps/disable-computerrestore.html  
 
 Disable-MicrosoftUpdate # until this is over
 Disable-BingSearch # forever
@@ -47,50 +47,6 @@ try {
  
   #Write-Output "Modifying taskbar options"
   Set-BoxstarterTaskbarOptions -Dock Bottom -Combine Always -AlwaysShowIconsOn
-  
-  # disabled bing search in start menu
-  #Write-Output "Disabling Bing Search in start menu"
-  #Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" -Name "BingSearchEnabled" -Type DWord -Value 0
-  #If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search")) {
-  #    New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Force | Out-Null
-  #}
-  #Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Name "DisableWebSearch" -Type DWord -Value 1
-
-  # Disable Cortana
-  #Write-Output "Disabling Cortana"
-  #If (!(Test-Path "HKCU:\SOFTWARE\Microsoft\Personalization\Settings")) {
-  #    New-Item -Path "HKCU:\SOFTWARE\Microsoft\Personalization\Settings" -Force | Out-Null
-  #}
-  #Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Personalization\Settings" -Name "AcceptedPrivacyPolicy" -Type DWord -Value 0
-  #If (!(Test-Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization")) {
-  #    New-Item -Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization" -Force | Out-Null
-  #}
-  #Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization" -Name "RestrictImplicitTextCollection" -Type DWord -Value 1
-  #Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization" -Name "RestrictImplicitInkCollection" -Type DWord -Value 1
-  #If (!(Test-Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization\TrainedDataStore")) {
-  #    New-Item -Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization\TrainedDataStore" -Force | Out-Null
-  #}
-  #Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization\TrainedDataStore" -Name "HarvestContacts" -Type DWord -Value 0
-  #If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search")) {
-  #    New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Force | Out-Null
-  #}
-  #Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Name "AllowCortana" -Type DWord -Value 0
-
-  # Hide taskbar search box
-  #Write-Output "Hiding task bar search box"
-  #Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" -Name "SearchboxTaskbarMode" -Type DWord -Value 0
-
-  # Hide Task View
-  #Write-Output "Hiding task view"
-  #Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ShowTaskViewButton" -Type DWord -Value 0
-
-  # Hide task Bar People icon
-  #Write-Output "Hiding task bar people icon"
-  #If (!(Test-Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People")) {
-  #    New-Item -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People" | Out-Null
-  #}
-  #Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People" -Name "PeopleBand" -Type DWord -Value 0
-
 
   ######################################################
   # Installing Dev Tools
